3c242cc04c47d810b1b1eedf6f5e7e53a0b1a321
Assem Mohamed <129564950+Assem29@users.noreply.github.com>
2023-04-21 04:07:46 +0200

    'task done'

--- js/app.js
+++ js/app.js
@@ -1,16 +1,44 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+var savedHtmlCode = localStorage.getItem("htmlCode") || "<div>\n\n</div>";
+var savedCssCode = localStorage.getItem("cssCode") || "<style>\n\n</style>";
+var savedJsCode = localStorage.getItem("jsCode") || "<script>\n\n</script>";
+
+document.getElementById("htmlCode").value = savedHtmlCode;
+document.getElementById("cssCode").value = savedCssCode;
+document.getElementById("jsCode").value = savedJsCode;
+
+document.getElementById("htmlCode").addEventListener("input", function() {
+  var htmlCode = document.getElementById("htmlCode").value;
+  localStorage.setItem("htmlCode", htmlCode);
+});
+
+document.getElementById("cssCode").addEventListener("input", function() {
+  var cssCode = document.getElementById("cssCode").value;
+  localStorage.setItem("cssCode", cssCode);
+});
+
+document.getElementById("jsCode").addEventListener("input", function() {
+  var jsCode = document.getElementById("jsCode").value;
+  localStorage.setItem("jsCode", jsCode);
+});
 
 function showPreview(){
     var htmlCode = document.getElementById("htmlCode").value;
     var cssCode = ""+document.getElementById("cssCode").value+"";
     var jsCode = ""+document.getElementById("jsCode").value+"";
+
+    localStorage.setItem("htmlCode", htmlCode);
+    localStorage.setItem("cssCode", cssCode);
+    localStorage.setItem("jsCode", jsCode);
+
     var frame = document.getElementById("preview-window").contentWindow.document;
     frame.open();
     frame.write(htmlCode+cssCode+jsCode);
     frame.close();
 }
+
+window.onload = function() {
+  showPreview();
+};
 
 function show(x){
     document.getElementById("html").style.display="none";
@@ -34,4 +62,4 @@ function show_all(){
         document.getElementById("js").style.display="none";
         document.getElementById("result").style.display="none";
     }
-}
+}  
